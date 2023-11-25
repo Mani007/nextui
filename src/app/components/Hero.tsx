2617b51c87e216fd878fd94e6f18fb69f019035f
Mohnish <mohnish.a007@gmail.com>
2023-11-25 18:11:36 +0000

    text over image hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <>
     <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+  <div className="container relative mx-auto flex px-5 py-24 items-center justify-center flex-col">
     {/* <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"> */}
     {/* <Image
       src="/images/opscraftherosection.png"
@@ -18,7 +18,7 @@ function Hero() {
       height={1070}
       alt="Picture of the author"
     />
-    <div className="text-center lg:w-2/3 w-full">
+    <div className="text-center absolute lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
       <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
       <div className="flex justify-center">
