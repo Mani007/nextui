f75b011ca715c3b70914c2453061568ae59219ba
Mohnish <mohnish.a007@gmail.com>
2023-12-03 01:19:04 +0000

    card center

--- src/app/components/Mcard.tsx
+++ src/app/components/Mcard.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './Card'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 function Mcard() {
   return (
     <>
-    <div className="container mx-auto flex px-5 py-7 md:flex-row flex-col items-center">
+    <div className="container justify-center mx-auto flex px-5 py-7 md:flex-row flex-col items-center">
+        {/* <Carousel showArrows={true} > */}
         <div className='mx-3'>
             <Card/>
         </div>
@@ -14,7 +16,7 @@ function Mcard() {
         <div className='mx-3'>
             <Card/>
         </div>
-        
+        {/* </Carousel> */}
         
     </div>
     </>
