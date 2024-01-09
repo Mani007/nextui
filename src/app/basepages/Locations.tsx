5538027d94dbf87973d8908be96f1c535458fe9a
Mohnish <mohnish.a007@gmail.com>
2024-01-09 04:24:24 +0000

    worldmap working

--- src/app/basepages/Locations.tsx
+++ src/app/basepages/Locations.tsx
@@ -1,29 +1,62 @@
-import React from 'react'
+import React from "react";
 import WorldMap from "react-svg-worldmap";
+import type { CountryContext } from "react-svg-worldmap";
 function Locations() {
-    const data = [
-        { country: "cn", value: 1389618778 }, // china
-        { country: "in", value: 1311559204 }, // india
-        { country: "us", value: 331883986 }, // united states
-        { country: "id", value: 264935824 }, // indonesia
-        { country: "pk", value: 210797836 }, // pakistan
-        { country: "br", value: 210301591 }, // brazil
-        { country: "ng", value: 208679114 }, // nigeria
-        { country: "bd", value: 161062905 }, // bangladesh
-        { country: "ru", value: 141944641 }, // russia
-        { country: "mx", value: 127318112 }, // mexico
-      ];
+  const width = Math.min(window.innerHeight, window.innerWidth) * 0.75;
+  const data = [
+    { country: "in", value: 1 }, // india
+    { country: "us", value: 1 }, // united states
+    { country: "gb", value: 1 }, // United Kingdom
+    { country: "de", value: 1 }, // Dermany
+  ];
+
+  function localizeNumber(num: number | undefined, digits: number) {
+    if (typeof num === "undefined") return "";
+    const magnitude = [
+      { value: 1, text: " India " },
+      { value: 1, text: " us" },
+      { value: 1, text: " uk " },
+      { value: 1, text: " de " },
+    ].find((magnitude) => num >= magnitude.value);
+    if (magnitude) {
+      return (
+        (num / magnitude.value)
+          .toFixed(digits)
+          .replace(/\.0+$|(?<number>\.[0-9]*[1-9])0+$/, "$1") + magnitude.text
+      );
+    }
+    return "";
+  }
+  const localizedCountryDictionary = new Map<string, string>([
+    ["in", "India"], // India
+    ["de", "Germany"], // Mexico
+    ["us", "United States"], // Nigeria
+    ["gb", "United Kingdom"], // Russia
+  ]);
+
+  const getLocalizedText = ({
+    countryCode,
+    countryValue,
+    prefix,
+    suffix,
+  }: CountryContext) =>
+    `${
+      localizedCountryDictionary.get(countryCode.toLocaleLowerCase()) ??
+      "Unknown"
+    }: ${prefix}${localizeNumber(countryValue, 2)}${suffix}`;
+
   return (
     <>
-     <WorldMap
-        color="red"
-        title="Top 10 Populous Countries"
-        value-suffix="people"
-        size="lg"
-        data={data}
-      />
+      <div>
+        <WorldMap
+          color="#29C3C3"
+          value-suffix="people"
+          size={width}
+          data={data}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default Locations
+export default Locations;
