18f167706a07f10ad4b60e8bdbe02ffba5d3cbe0
Mohnish <mohnish.a007@gmail.com>
2024-01-14 00:40:15 +0000

    original config od ts and next as before next-video

--- src/app/components/Bgvideo.tsx
+++ src/app/components/Bgvideo.tsx
@@ -3,12 +3,13 @@ import Video from 'next-video';
 import myVideo from '../../../public/Videos/IntroBGopscraft.mp4';
 function Bgvideo() {
     // You don't have to pass full path for video src
+
   return (
    <>
    
-   <video src="Videos/IntroBGopscraft.mp4" autoPlay loop muted playsInline className='w-screen h-screen z-10'>
+   <video src="Videos/IntroBGopscraft.mp4" autoPlay loop muted playsInline className='w-screen h-screen z-10'/>
              
-            </video>
+            
    
    </>
   )
