be376b241f31072d22234327a0351ef288333147
Mohnish <mohnish.a007@gmail.com>
2024-01-15 19:55:03 +0000

    bgvideo updated

--- src/app/components/Bgvideo.tsx
+++ src/app/components/Bgvideo.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Video from 'next-video';
 import myVideo from '../../../public/Videos/IntroBGopscraft.mp4';
 function Bgvideo() {
     // You don't have to pass full path for video src
 
   return (
    <>
-   <div className="container relative mx-0 my-0 flex px-5 py-5 items-center justify-center flex-col">
+   <div className="container relative  flex  items-center justify-center flex-col">
     {/* <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"> */}
     {/* <Image
       src="/images/opscraftherosection.png"
@@ -14,14 +13,16 @@ function Bgvideo() {
       height={600}
       alt="Picture of the author"
     /> */}
-    <video src="Videos/IntroBGopscraft.mp4" autoPlay loop muted playsInline className='w-full h-screen '/>
-    <div className="text-center absolute lg:w-2/3 w-full">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
-      <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
-      <div className="flex justify-center">
+    <div className='w-screen'>
+
+    <video src="Videos/IntroBGopscraft.mp4" autoPlay loop muted playsInline  className=' '/>
+    </div>
+    <div className="text-center absolute lg:w-4/5 w-full">
+      <h1 className="title-font sm:text-6xl text-xl mb-4 font-medium text-slate-200">OPScraft is the Last thing you ever need in <span className='text-[#00df9a]'>Artificial Intelligence</span></h1>
+      {/* <div className="flex justify-center">
         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
         <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
-      </div>
+      </div> */}
     </div>
   </div>
   
